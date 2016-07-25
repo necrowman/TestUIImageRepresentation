ef3a2f46a1f71612d769fd0ef1e560ea1c1311a8
Ruslan Yupyn <r.yupyncrossplusplus.org>
2016-07-25 22:11:02 +0300

    simple test

---
+++ TestImageDecodingTests/TestImageDecodingTests.swift
@@ -0,0 +1,45 @@
+//
+//  TestImageDecodingTests.swift
+//  TestImageDecodingTests
+//
+//  Created by Ruslan Yupyn on 7/25/16.
+//  Copyright © 2016 Crossroad Labs. All rights reserved.
+//
+
+import XCTest
+import Foundation
+@testable import TestImageDecoding
+
+class TestImageDecodingTests: XCTestCase {
+    let imageURLString = "https://cdn.photographylife.com/wp-content/uploads/2014/06/Nikon-D810-Image-Sample-6.jpg"
+    var image: UIImage?
+    override func setUp() {
+        super.setUp()
+        let url:NSURL? = NSURL(string: self.imageURLString)
+        let data:NSData? = NSData(contentsOfURL : url!)
+        image = UIImage(data : data!)
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            for _ in 0..<15 {
+                let data = UIImageJPEGRepresentation(self.image!, 0.35)
+                print("size -> ", data?.length)
+            }
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
